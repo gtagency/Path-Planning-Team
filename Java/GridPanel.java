3fce38206d303f9c4729ffff331c8bfe8cfcde69
Thomas Shields <thomas.shields@gatech.edu>
2013-03-19 17:23:15 -0400

    Java A* visualization working now. Turns out I just forgot to check if a node was in the PQ, resulting in 1. Terrible runtime, because nodes were getting hit a billion times 2. Terrible paths, because good paths were getting overwritten.
    Anyways, it works now.
    -T

--- Java/GridPanel.java
+++ Java/GridPanel.java
@@ -6,23 +6,34 @@ public class GridPanel extends JPanel {
     private Location[][] grid;
     private int height, width;
     private ArrayList<Location> path;
+    private ArrayList<Location> currentPath;
+    private final int MULTIPLIER = 20;
 
+
+    /**
+     * Sets up a grid panel and all, ya know?
+     * @param rows      The number of rows for this grid panel thingy
+     * @param cols      The number of columns. I hate javadocs.
+     */
     public GridPanel(int rows, int cols) {
-        height = 20*rows;
-        width = 20*cols;
+
+        //multiply by MULTIPLIER for better visualization
+        height = MULTIPLIER*rows;
+        width = MULTIPLIER*cols;
         Random costGenerator = new Random();
         grid = new Location[rows][cols];
         for(int x=0;x<grid.length;x++) {
             for(int y=0;y<grid[x].length;y++) {
-                grid[x][y] = new Location(new Point(x,y), (costGenerator.nextInt(10) + 1));
+                grid[x][y] = new Location(x,y, (costGenerator.nextInt(8) + 3));
             }
         }
-        grid[rows-1][cols-1] = new Location(new Point(rows-1, cols-1), 1);
+        grid[rows-1][cols-1] = new Location(rows-1, cols-1, 1);
 
 
         JFrame jframe = new JFrame();
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(20*(rows+1), 20*(cols+1)));
+        setPreferredSize(new Dimension(MULTIPLIER*(rows+1),
+                                        MULTIPLIER*(cols+1)));
         jframe.getContentPane().add(this);
         jframe.pack();
         jframe.setVisible(true);
@@ -33,35 +44,73 @@ public class GridPanel extends JPanel {
        // BFS(grid[0][0], grid[rows-1][cols-1]);
     }
 
+    /**
+     * PAINT ALL THE THINGS
+     */
     public void paint(Graphics g) {
+
+        // paint the map
         g.clearRect(0, 0, width, height);
 
         g.setColor(new Color(0,0,0));
         for(int x=0;x<grid.length;x++) {
             for(int y=0;y<grid[x].length;y++) {
-                g.fillOval(20*x, 20*y, (int)(15*grid[x][y].OBSTACLE_PROBABILITY/10.0), (int)(15*grid[x][y].OBSTACLE_PROBABILITY/10.0));
+                g.fillOval(MULTIPLIER*x, MULTIPLIER*y, 
+                    (int)(15*grid[x][y].getObstacleProbability()/10.0), 
+                    (int)(15*grid[x][y].getObstacleProbability()/10.0));
             }
         }
 
+        // uncomment to paint all paths tried
+        // g.setColor(Color.BLUE);
+        // for(int i=0;i<path.size()-1;i++) {
+        //     int x1 = path.get(i).getX(),
+        //         y1 = path.get(i).getY(),
+        //         x2 = path.get(i+1).getX(),
+        //         y2 = path.get(i+1).getY();
 
+        //     g.drawLine(MULTIPLIER*x1, MULTIPLIER*y1, MULTIPLIER*x2, MULTIPLIER*y2);
+        //     g.fillOval(MULTIPLIER*x1, MULTIPLIER*y1, 
+        //         (int)(15*grid[x1][y1].getObstacleProbability()/10.0), 
+        //         (int)(15*grid[x1][y1].getObstacleProbability()/10.0));
+        //     g.fillOval(MULTIPLIER*x2, MULTIPLIER*y2, 
+        //         (int)(15*grid[x2][y2].getObstacleProbability()/10.0), 
+        //         (int)(15*grid[x2][y2].getObstacleProbability()/10.0));
+
+        // }
+
+
+        // paint last path in red
         g.setColor(Color.RED);
-        for(int i=0;i<path.size()-1;i++) {
-            int x1 = (int)(path.get(i).point.getX()),
-                y1 = (int)(path.get(i).point.getY()),
-                x2 = (int)(path.get(i+1).point.getX()),
-                y2 = (int)(path.get(i+1).point.getY());
+        for(int i=0;i<currentPath.size()-1;i++) {
+            int x1 = currentPath.get(i).getX(),
+                y1 = currentPath.get(i).getY(),
+                x2 = currentPath.get(i+1).getX(),
+                y2 = currentPath.get(i+1).getY();
 
-            // System.out.printf("Drawing line from (%d, %d) to (%d, %d)\n", x1, y1, x2, y2);
-            g.drawLine(20*x1, 20*y1, 20*x2, 20*y2);
-            g.fillOval(20*x1, 20*y1, (int)(15*grid[x1][y1].OBSTACLE_PROBABILITY/10.0), (int)(15*grid[x1][y1].OBSTACLE_PROBABILITY/10.0));
-            g.fillOval(20*x2, 20*y2, (int)(15*grid[x2][y2].OBSTACLE_PROBABILITY/10.0), (int)(15*grid[x2][y2].OBSTACLE_PROBABILITY/10.0));
+            g.drawLine(MULTIPLIER*x1, MULTIPLIER*y1, 
+                        MULTIPLIER*x2, MULTIPLIER*y2);
+            g.fillOval(MULTIPLIER*x1, MULTIPLIER*y1, 
+                (int)(15*grid[x1][y1].getObstacleProbability()/10.0), 
+                (int)(15*grid[x1][y1].getObstacleProbability()/10.0));
+            g.fillOval(MULTIPLIER*x2, MULTIPLIER*y2, 
+                (int)(15*grid[x2][y2].getObstacleProbability()/10.0), 
+                (int)(15*grid[x2][y2].getObstacleProbability()/10.0));
 
         }
     }
+
+    /** 
+     * Heuristic between two things
+     */
     public int heuristic(Location start, Location end) {
-        return 1; //(int)(start.point.distance(end.point));
+        return (int)(Math.sqrt(Math.pow(start.getX() - end.getX(),2) 
+                    + Math.pow(start.getY() - end.getY(),2)));
     }
 
+    /**
+     * a* all the things!
+     */
     public ArrayList<Location> aStar(Location start, Location end) {
         // array of visited nodes
         ArrayList<Location> visited = new ArrayList<Location>();
@@ -73,37 +122,42 @@ public class GridPanel extends JPanel {
         nodes.add(start);
 
         //likewise, the best path is the one that starts at the start
+        path.clear();
         path.add(start);
-
         //set the path on start
         start.setPathTo((ArrayList<Location>)path.clone());
+        currentPath = (ArrayList<Location>)start.getPathTo().clone();
 
         //set the cost (without a heuristic)
-        start.setNonHeuristicCost(start.OBSTACLE_PROBABILITY);
+        start.setNonHeuristicCost(start.getObstacleProbability());
         while(!nodes.isEmpty()) {
             Location current = nodes.poll();
+            visited.add(current);
+            path.add(current);
+            currentPath = (ArrayList<Location>)current.getPathTo().clone();
             if(current.equals(end)) {
-                repaint();
                 System.out.println("Success from if statement");
-                path.add(current);
+                // path.add(current);
+                repaint();
                 return path;
             }
-            visited.add(current);
-            ArrayList<Location> children = getNodeChildren(current, visited);
+            ArrayList<Location> children = getNodeChildren(current, visited, 
+                                            new ArrayList(nodes));
             for(Location child : children) {
-                child.setCost(child.OBSTACLE_PROBABILITY 
-                                + current.getNonHeuristicCost()
+                child.setCost(child.getObstacleProbability() 
+                               + current.getNonHeuristicCost()
                                 + heuristic(child, end)
                                 );
 
-                child.setNonHeuristicCost(child.OBSTACLE_PROBABILITY 
+                child.setNonHeuristicCost(child.getObstacleProbability() 
                                 + current.getNonHeuristicCost()
                                 // + heuristic(child, end)
                                 );
 
-                path = (ArrayList<Location>)current.getPathTo().clone();
-                path.add(child);
-                child.setPathTo((ArrayList<Location>)path.clone());
+                ArrayList<Location> childPath = 
+                            ((ArrayList<Location>)current.getPathTo().clone());
+                childPath.add(child);
+                child.setPathTo((ArrayList<Location>)childPath.clone());
                 nodes.add(child);
             }
             repaint();
@@ -113,11 +167,6 @@ public class GridPanel extends JPanel {
             catch(InterruptedException ex) {
                 System.out.println("Yo dawg quit crashing the program");
             }
-            // long startTime = System.nanoTime(),
-            //     now;
-            // do {
-            //     now = System.nanoTime();
-            // }while((now - startTime) < 500000000l);
         }
         repaint();
         System.out.println("Success");
@@ -125,138 +174,47 @@ public class GridPanel extends JPanel {
     }
 
 
-    private ArrayList<Location> getNodeChildren(Location node, ArrayList<Location> alreadyVisited) {
+    private ArrayList<Location> getNodeChildren(Location node, 
+            ArrayList<Location> alreadyVisited, ArrayList<Location> stack) {
         ArrayList<Location> result = new ArrayList<Location>();
-        int x = (int)(node.point.getX()),
-            y = (int)(node.point.getY());
-        result = addLocation(x-1,y-1, result, alreadyVisited);
-        result = addLocation(x-1,y, result, alreadyVisited);
-        result = addLocation(x-1,y+1, result, alreadyVisited);
-        result = addLocation(x,y-1, result, alreadyVisited);
-        result = addLocation(x,y+1, result, alreadyVisited);
-        result = addLocation(x+1,y-1, result, alreadyVisited);
-        result = addLocation(x+1,y, result, alreadyVisited);
-        result = addLocation(x+1,y+1, result, alreadyVisited);
+        int x = node.getX(),
+            y = node.getY();
+        result = addLocation(x-1,y-1, result, alreadyVisited, stack);
+        result = addLocation(x-1,y, result, alreadyVisited, stack);
+        result = addLocation(x-1,y+1, result, alreadyVisited, stack);
+        result = addLocation(x,y-1, result, alreadyVisited, stack);
+        result = addLocation(x,y+1, result, alreadyVisited, stack);
+        result = addLocation(x+1,y-1, result, alreadyVisited, stack);
+        result = addLocation(x+1,y, result, alreadyVisited, stack);
+        result = addLocation(x+1,y+1, result, alreadyVisited, stack);
         return result;
     }
 
-    private ArrayList<Location> addLocation(int x, int y, ArrayList<Location> nodes, ArrayList<Location> alreadyVisited) {
+    private ArrayList<Location> addLocation(int x, int y, 
+        ArrayList<Location> nodes, 
+        ArrayList<Location> alreadyVisited, 
+        ArrayList<Location> stack) {
         if(inGrid(x, y)) {
             Location loc = grid[x][y];
-            if(!alreadyVisited.contains(loc)) {
+            if(!alreadyVisited.contains(loc) && !stack.contains(loc)) {
                 nodes.add(loc);
             }
         }
         return nodes;
     }
+
+    /**
+     * Checks to see if a point is in the grid
+     * @param x     The x coordinate of the point to check
+     * @param y     The y coordinate ...
+     * @return true if the point is valid
+     */
     private boolean inGrid(int x, int y) {
         return x < grid.length && x >= 0 && y < grid[x].length && y >= 0;
     }
 
-    public ArrayList<Location> DFS(Location start, Location end) {
-        // array of visited nodes
-        ArrayList<Location> visited = new ArrayList<Location>();
 
-        //pq for nodes yet to be examined
-        Stack<Location> nodes = new Stack<Location>();
-
-        //the best place to begin is at the beginning
-        nodes.add(start);
-
-        //likewise, the best path is the one that starts at the start
-        path.add(start);
-
-        //set the path on start
-        start.setPathTo((ArrayList<Location>)path.clone());
-
-        while(!nodes.isEmpty()) {
-            Location current = nodes.pop();
-            if(current.equals(end)) {
-                repaint();
-                System.out.println("Success from if statement");
-                path.add(current);
-                return path;
-            }
-            visited.add(current);
-            ArrayList<Location> children = getNodeChildren(current, visited);
-            for(Location child : children) {
-                path = (ArrayList<Location>)current.getPathTo().clone();
-                path.add(child);
-                child.setPathTo((ArrayList<Location>)path.clone());
-                nodes.add(child);
-            }
-            repaint();
-            try {
-                Thread.sleep(100);
-            }
-            catch(InterruptedException ex) {
-                System.out.println("Yo dawg quit crashing the program");
-            }
-            // long startTime = System.nanoTime(),
-            //     now;
-            // do {
-            //     now = System.nanoTime();
-            // }while((now - startTime) < 500000000l);
-        }
-        repaint();
-        System.out.println("Success");
-        return path;
-   
-    }
-
-
-    public ArrayList<Location> BFS(Location start, Location end) {
-        // array of visited nodes
-        ArrayList<Location> visited = new ArrayList<Location>();
-
-        //pq for nodes yet to be examined
-        LinkedList<Location> nodes = new LinkedList<Location>();
-
-        //the best place to begin is at the beginning
-        nodes.add(start);
-
-        //likewise, the best path is the one that starts at the start
-        path.add(start);
-
-        //set the path on start
-        start.setPathTo((ArrayList<Location>)path.clone());
-
-        while(!nodes.isEmpty()) {
-            Location current = nodes.poll();
-            if(current.equals(end)) {
-                repaint();
-                System.out.println("Success from if statement");
-                path.add(current);
-                return path;
-            }
-            visited.add(current);
-            ArrayList<Location> children = getNodeChildren(current, visited);
-            for(Location child : children) {
-                path = (ArrayList<Location>)current.getPathTo().clone();
-                path.add(child);
-                child.setPathTo((ArrayList<Location>)path.clone());
-                nodes.add(child);
-            }
-            repaint();
-            try {
-                Thread.sleep(1);
-            }
-            catch(InterruptedException ex) {
-                System.out.println("Yo dawg quit crashing the program");
-            }
-            // long startTime = System.nanoTime(),
-            //     now;
-            // do {
-            //     now = System.nanoTime();
-            // }while((now - startTime) < 500000000l);
-        }
-        repaint();
-        System.out.println("Success");
-        return path;
-   
-    }
-
-    public static void main(String[] args) {
-        GridPanel panel = new GridPanel(10, 10);
+   public static void main(String[] args) {
+        GridPanel panel = new GridPanel(30, 30);
     }
 }
--- Java/Location.java
+++ Java/Location.java
@@ -1,46 +1,126 @@
 import java.awt.*;
 import java.util.*;
 
+/**
+ * Represents a location on the map
+ * Contains obstacle and cost data
+ * Also lets you set the path to it
+ * 
+ * @author Thomas Shields
+ * @version 2.0 3/19/13
+ */
+
 public class Location implements Comparable{
-    public final Point point;
-    public final double OBSTACLE_PROBABILITY;
+    public double obstacleProbability;
     private double cost;
     private double nonHeuristicCost;
     private ArrayList<Location> pathTo;
+    private int x, y;
 
-    public Location(Point point, double OBSTACLE_PROBABILITY) {
-        this.point = point;
-        this.OBSTACLE_PROBABILITY = OBSTACLE_PROBABILITY;
+    /**
+     * Constructor for a Location
+     * @param x         the x-coordinate of the location
+     * @param y         the y-coordinate of the location
+     * @param obstacleProbability   the probability of an obstacle at location
+     */
+    public Location(int x, int y, double obstacleProbability) {
+        this.x = x;
+        this.y = y;
+        this.obstacleProbability = obstacleProbability;
     }
 
+    /**
+     * Gets the x coordinate
+     * @return the x coordinate
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Gets the y coordinate
+     * @return the y coordinate
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Gets the probability of an obstacle at this location
+     * @return that probability. Obviously. Why would it be anythign diff?
+     */
+    public double getObstacleProbability() {
+        return obstacleProbability;
+    }
+
+    /**
+     * Gets the cost
+     * @return the cost with the heuristic factored in
+     */
     public double getCost() {
         return cost;
     }
 
+    /**
+     * Set the cost
+     * @param _cost     the cost to set it to
+     */
     public void setCost(double _cost) {
         cost = _cost;
     }
 
+    /**
+     * Gets the cost without heuristic
+     * @return the raw cost, without the heuristic
+     */
     public double getNonHeuristicCost() {
         return nonHeuristicCost;
     }
 
-    public void setNonHeuristicCost(double _cost) {
+    /**
+     * Set the non heuristic cost
+     * @param _cost     the cost to set it to
+     */    public void setNonHeuristicCost(double _cost) {
         nonHeuristicCost = _cost;
     }
 
-    public int compareTo(Object other) {
-        return (int)((cost - ((Location)other).cost)*10);
-    }
-
+    /**
+     * gets the path to this location
+     * @return the path to this location
+     */
     public ArrayList<Location> getPathTo() {
         return (ArrayList<Location>)pathTo.clone();
     }
+
+    /**
+     * Sets the path to this location
+     * @param path      The path
+     */
     public void setPathTo(ArrayList<Location> path) {
         pathTo = path;
     }
 
+    /**
+     * Checks equality yo
+     * @param obj       the object to check with
+     * @return true if equal
+     */
+    @Override
     public boolean equals(Object obj) {
-        return ((Location)obj).point.equals(point);
+        return obj instanceof Location ? 
+                    x == ((Location)obj).x && y == ((Location)obj).y
+                    : false;
+    }
+
+    /**
+     * Compares two objects yo
+     * @param other     the other thing obviously
+     * @return an integer comparison
+     */
+    @Override
+    public int compareTo(Object other) {
+        return other instanceof Location ? 
+                    (int)((cost - ((Location)other).cost)*10)
+                    : Integer.MAX_VALUE;
     }
 }
